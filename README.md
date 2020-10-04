022ae56b60cd4975ebd768018052d9e6b924dc35
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-10-04 07:32:44 -0600

    Added init to README

--- README.md
+++ README.md
@@ -8,10 +8,42 @@ Project for IvyHacks 2020 (Team Zyndie)
 
 ## Team
 
-**Filippo Fonseca** - _Full-Stack Engineer, Content_
+**Filippo Fonseca** - _Full-Stack Engineer, Content, Design_
 
-**Mark Music** - _Full-Stack Engineer, Design_
+**Mark Music** - _Full-Stack Engineer, Content, Design_
 
-**Daniel Giacomin** - _Backend Engineer_
+**Daniel Giacomin** - _Backend Engineer, Content_
 
-___
+---
+
+## Initialization
+
+If you'd like to clone the repo and make changes/review it/provide feedback, feel free to follow the steps outlined below:
+
+1. Clone the repo
+
+```
+git clone git@github.com:filippo-fonseca/avoid.git
+```
+
+2. Change dorectories
+
+```
+cd avoid
+```
+
+3. Install all dependencies
+
+```ts
+npm install
+// or
+yarn
+```
+
+4. Start the development server
+
+```ts
+npm run dev
+// or
+yarn dev
+```
--- pages/nycmap.tsx
+++ pages/nycmap.tsx
@@ -1,19 +1,25 @@
 import React from "react";
 import NYCMapView from "./maps/nycmapview";
 import CityToggle from "./maps/city-toggle/nyc/CityToggle";
+import MapHeader from "./header/HeaderMap";
+import { Container, Polygon, FrameDiv } from "../public/css/SeattleMap";
 
 const DisplayNYCMap = () => {
   return (
-    <div>
-      <iframe
-        src="https://www.google.com/maps/d/u/0/embed?mid=16guXLAS8awdBQrE4G_ONfEcY2YVgswBK"
-        width="1400"
-        height="832"
-      ></iframe>
-      <div>
-        <CityToggle />
-      </div>
-    </div>
+    <Container>
+      <MapHeader />
+      <FrameDiv>
+        <Polygon>
+          <iframe
+            src="https://www.google.com/maps/d/u/0/embed?mid=16guXLAS8awdBQrE4G_ONfEcY2YVgswBK"
+            width="1400"
+            height="808"
+          ></iframe>
+        </Polygon>
+      </FrameDiv>
+
+      <CityToggle />
+    </Container>
   );
 };
 
