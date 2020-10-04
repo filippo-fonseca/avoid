736dc61eb8ec143e61ceda86f92ae17f47805ab4
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-10-04 06:53:41 -0600

    Added Header component to map

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,7 @@ import DisplaySeattleMap from "./seattlemap";
 import DisplayNYCMap from "./nycmap";
 
 const Index = () => {
-  return (
-    <div>
-      <iframe
-        src="https://www.google.com/maps/d/u/0/embed?mid=10YQ_lUpkXGWIJFYv07JC65TatPZTRXrZ"
-        width="640"
-        height="480"
-      ></iframe>
-    </div>
-  );
+  return <div>Hello World</div>;
 };
 
 export default Index;
--- pages/seattlemap.tsx
+++ pages/seattlemap.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import SeattleMapView from "./maps/seattlemapview";
 import NYCMapView from "./maps/nycmapview";
 import CityToggle from "./maps/city-toggle/seattle/CityToggle";
+import MapHeader from "./header/HeaderMap";
+import { Container, Polygon, FrameDiv } from "../public/css/SeattleMap";
 
 const DisplaySeattleMap = () => {
   return (
-    <div>
-      <iframe
-        src="https://www.google.com/maps/d/u/0/embed?mid=10YQ_lUpkXGWIJFYv07JC65TatPZTRXrZ"
-        width="1400"
-        height="832"
-      ></iframe>
-      <div>
-        <CityToggle />
-      </div>
-    </div>
+    <Container>
+      <MapHeader />
+      <FrameDiv>
+        <Polygon>
+          <iframe
+            src="https://www.google.com/maps/d/u/0/embed?mid=10YQ_lUpkXGWIJFYv07JC65TatPZTRXrZ"
+            width="1400"
+            height="808"
+          ></iframe>
+        </Polygon>
+      </FrameDiv>
+
+      <CityToggle />
+    </Container>
   );
 };
 
